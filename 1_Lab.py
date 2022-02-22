5f0487908d6897bcfca6be89c10646d353c0e2c0
ivanlima@misterivanlima.com <ivanlima@misterivanlima.com>
2022-02-21 21:32:10 -0500

    First Lab

---
+++ 1_Lab.py
@@ -0,0 +1,151 @@
+# Try your first Python output
+import sys
+print('Hello, Python2!')
+
+# Check the Python Version
+
+
+print(sys.version)
+
+# Practice on writing comments
+
+print('Hello, Python!') # This line prints a string
+# print('Hi')
+
+# Print string as error message
+#frint("Hello, Python!")
+
+# Try to see built-in error message
+#print("Hello, Python!)
+
+
+# Print string and error to see the running order
+
+print("This will be printed")
+#frint("This will cause an error")
+print("This will NOT be printed")
+
+# Integer
+11
+
+# Float
+2.14
+
+# String
+"Hello, Python 101!"
+
+# Type of 12
+type(12)
+
+# Type of 2.14
+type(2.14)
+
+# Type of "Hello, Python 101!"
+type("Hello, Python 101!")
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+type(12.0)
+
+# Print the type of -1
+type(-1)
+
+# Print the type of 4
+type(4)
+
+# Print the type of 0
+type(0)
+
+# Print the type of 1.0
+type(1.0) # Notice that 1 is an int, and 1.0 is a float
+
+# Print the type of 0.5
+type(0.5)
+
+# Print the type of 0.56
+type(0.56)
+
+# System settings about float type
+sys.float_info
+
+# Convert 2 to a float
+float(2)
+
+# Convert integer 2 to a float and check its type
+type(float(2))
+
+# Casting 1.1 to integer will result in loss of information
+int(1.1)
+
+# Convert a string into an integer
+int('1')
+
+# Convert a string into an integer with error
+#int('1 or 2 people')
+
+# Convert the string "1.2" into a float
+float('1.2')
+
+# Convert an integer to a string
+str(1)
+
+# Convert a float to a string
+str(1.2)
+
+# Value true
+True
+
+# Value false
+False
+
+# Type of True
+type(True)
+
+# Type of False
+type(False)
+
+# Convert True to int
+int(True)
+
+# Convert 1 to boolean
+bool(1)
+
+# Convert 0 to boolean
+bool(0)
+
+# Convert True to float
+float(True)
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+type(6 / 2)
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+type(6 // 2)
+
+# Store value into variable
+x = 43 + 60 + 16 + 41
+x
+
+# Use another variable to store the result of the operation between variable and value
+y = x / 60
+y
+
+# Name the variables meaningfully
+total_min = 43 + 42 + 57 # Total length of albums in minutes
+total_min
+
+
+# Complicate expression
+total_hours = (43 + 42 + 57) / 60  # Total hours in a single expression
+total_hours
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+x = 3 + 2 * 2
+x
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+y = (3 + 2) * 2
+y
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+z = x + y
+z
